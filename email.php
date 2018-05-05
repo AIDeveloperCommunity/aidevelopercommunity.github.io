cb73cba3d19817ea8bda670c0b5b902eada4da0d
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2018-05-06 02:03:14 +0530

    added email

---
+++ email.php
@@ -0,0 +1,22 @@
+<?php
+$subject = $_REQUEST['subject'] . ' Ajax HTML Contact Form : Demo'; // Subject of your email
+$to = 'info@designesia.com';  //Recipient's E-mail
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+$message .= 'Name: ' . $_REQUEST['name'] . "<br>";
+$message .= $_REQUEST['message'];
+
+if (@mail($to, $subject, $message, $headers))
+{
+	// Transfer the value 'sent' to ajax function for showing success message.
+	echo 'sent';
+}
+else
+{
+	// Transfer the value 'failed' to ajax function for showing error message.
+	echo 'failed';
+}
+?>
